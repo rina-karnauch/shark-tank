2c59a06ea2310db62e5485b7226b5ee2168e1289
rina.karnauch <rina.karnauch@mail.huji.ac.il>
2022-04-12 01:01:19 +0300

    fixed pic i think

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,19 @@
 import * as React from 'react';
-import './Footer.css'
+import './Footer.css';
+import logo from '../images/logo.png';
 
 function Footer() {
     return (
         <footer className="site-footer">
-            This site was created by Rina Karnauch, Ido Pinto, Yohai Shohet, I.B.D and Daniel Perretz with the assistance of Eliran from Lightricks. We love you🧚🏻.
-            <br/>
-            All Rights Reserved © 2022
+            {/*This site was created by Rina Karnauch, Ido Pinto, Yohai Shohet, I.B.D and Daniel Perretz with the assistance of Eliran from Lightricks. We love you🧚🏻.*/}
+            {/*<br/>*/}
+            {/*All Rights Reserved © 2022*/}
+            <img
+                 src={logo}
+                 alt="student-photo"
+                 width="50px"
+                 height="50px"
+            />
         </footer>
     );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -23,21 +23,30 @@ function Home() {
                 </Grid>
                 <Grid item xs={12}>
                     <h1 className="h1-big">
-                        SharkTank is not just a matching app. <br/>
-                        You deserve to find the best shark partner for you in the aquarium. <br/>
+                        <b> SharkTank is not just a matching app.</b> <br/>
+                        You deserve to find the best shark partner for you in the aquarium,
+                        so join us and start hunting<br/>
                     </h1>
                 </Grid>
                 <Grid item xs={12}>
                     <div className="join-box"/>
                 </Grid>
+                <Grid item xs={12}>
+                    <div className="h1-info">
+                        This site was created by Rina Karnauch, Ido Pinto, Yohai Shohet, I.B.D and Daniel Perretz with
+                        the assistance of Eliran from Lightricks. We love you🧚🏻.
+                        <br/>
+                        All Rights Reserved © 2022
+                    </div>
+                </Grid>
                 <img className="students-image"
                      src={students1}
                      alt="student-photo"
                      style={{
-                         position:"sticky",
-                         left:900,
-                         bottom:100,
-                         zIndex: "2",
+                         position: "fixed",
+                         left: 700,
+                         bottom: 75,
+                         zIndex: "3",
                      }}
                 />
             </Container>
